dcc4a645ef3dfeb08a8cb02feb99990bbfb49eb3
Dody Harianto <71028982+dodyharianto@users.noreply.github.com>
2023-06-18 14:54:16 +0700

    Add content for the "pricing" menu.

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import folium
+from streamlit_folium import folium_static
 plt.style.use('fivethirtyeight')
 nltk.download('stopwords')
 
@@ -29,6 +31,21 @@ def set_page_configuration():
 def display_introduction():
     st.title('PDF Insight 📚')
     st.write('Get the summary of your long PDF files in no time.')
+
+def get_selected_navbar_menu():
+    selected_navbar_menu = option_menu(
+        menu_title = None, # required
+        options = ['Home', 'Product', 'Pricing', 'Contact Us'],
+        icons = ['house', 'bag', 'currency-dollar', 'telephone'],
+        menu_icon = 'cast',
+        default_index = 0,
+        orientation = 'horizontal',
+        styles = {'nav-link': {'--hover-color': '#fc6b03',
+                               '--active-background-color': '#eee'},
+        'nav-link-selected': {'background-color': '#eb3502'},
+        })
+
+    return selected_navbar_menu
 
 def remove_stopwords(text):
     factory = StopWordRemoverFactory()
@@ -74,20 +91,15 @@ def generate_word_cloud(df):
     plt.axis('off')
     plt.show()
 
-def plot_n_grams(df, n):
-    # Combine all text into a single string
-    text = ' '.join(df['text'])
-
-    # Generate n-grams using the specified value of n
-    ngrams = nltk.ngrams(text.split(), n)
-    ngram_counts = Counter(ngrams)
-
-    # Create a dataframe with n-gram counts
-    top_ngrams = pd.DataFrame(ngram_counts.most_common(25), columns=['Words', 'Count'])
+def plot_n_grams(df):
+    data = df['text'].apply(lambda x: str(x).split())
+    top = Counter([item for sublist in data for item in sublist])
+    top_ngrams = pd.DataFrame(top.most_common(25))
+    top_ngrams.columns = ['Words', 'Count']
     st.dataframe(top_ngrams.style.background_gradient(cmap='Oranges'))
 
-    with st.expander('View the bar chart'):
-        words = [' '.join(words) for words, _ in top_ngrams['Words']]
+    with st.expander('View most common words in more detail here'):
+        words = top_ngrams['Words']
         counts = top_ngrams['Count']
 
         fig = go.Figure(data=go.Bar(
@@ -102,47 +114,44 @@ def plot_n_grams(df, n):
         ))
 
         fig.update_layout(
-            title=f'Top {n}-Grams',
+            title='Top N-Grams',
             xaxis=dict(title='Count'),
             yaxis=dict(title='Words'),
-            height=800
+            height=750
         )
 
         fig.update_yaxes(autorange='reversed')
+
+        # Render the plotly figure using Streamlit
         st.plotly_chart(fig, use_container_width=True)
 
+def display_home():
+    st.header('Unleash the Power of Data with Our PDF Scraping Solutions')
+    st.markdown('<br>', unsafe_allow_html=True)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write('Take your data analysis to the next level with our cutting-edge PDF scraping technology, and start your free trial today to experience the power of actionable insights at your fingertips.')
+        st.write('With our PDF scraping solutions, you can unlock valuable data hidden within PDF files and leverage it for in-depth analysis. Whether you need to extract textual information, perform sentiment analysis, or uncover trends and patterns, our cutting-edge technology has got you covered. By automating the extraction process, we save you valuable time and effort, allowing you to focus on deriving actionable insights from your data.')
+    with col2:
+        st.image('home-image.png', use_column_width=True)
 
-# def plot_n_grams(df):
-#     data = df['text'].apply(lambda x: str(x).split())
-#     top = Counter([item for sublist in data for item in sublist])
-#     top_ngrams = pd.DataFrame(top.most_common(25))
-#     top_ngrams.columns = ['Words', 'Count']
-#     st.dataframe(top_ngrams.style.background_gradient(cmap='Oranges'))
+    st.markdown('<br><br><br><br>', unsafe_allow_html=True)
+    st.markdown('<h3 style="text-align:center">Why Choose Us?</h3>', unsafe_allow_html=True)
 
-#     with st.expander('View the bar chart'):
-#         words = top_ngrams['Words']
-#         counts = top_ngrams['Count']
-
-#         fig = go.Figure(data=go.Bar(
-#             x=counts,
-#             y=words,
-#             marker=dict(
-#                 color=counts,
-#                 colorbar=dict(title='Count'),
-#                 colorscale='YlOrRd',
-#             ),
-#             orientation='h'
-#         ))
-
-#         fig.update_layout(
-#             title='Top N-Grams',
-#             xaxis=dict(title='Count'),
-#             yaxis=dict(title='Words'),
-#             height = 800
-#         )
-
-#         fig.update_yaxes(autorange = 'reversed')
-#         st.plotly_chart(fig, use_container_width=True)
+    st.markdown('<br>', unsafe_allow_html=True)
+    col1, col2, col3, col4 = st.columns(4)
+    with col1:
+        st.markdown('<h4>99.9%</h4>', unsafe_allow_html=True)
+        st.write('Uptime over the last 3 months.')
+    with col2:
+        st.markdown('<h4>2 mins.</h4>', unsafe_allow_html=True)
+        st.write('Help response averages.')
+    with col3:
+        st.markdown('<h4>+100K</h4>', unsafe_allow_html=True)
+        st.write('PDF scraped monthly.')
+    with col4:
+        st.markdown('<h4>>90%</h4>', unsafe_allow_html=True)
+        st.write('Customer are happy with the results.')
 
 def display_product():
     uploaded_file = st.file_uploader('Choose a PDF file', accept_multiple_files = False)
@@ -161,13 +170,13 @@ def display_product():
         df = pd.DataFrame({'text': text_list})
         csv_filename = f'{os.path.splitext(filename)[0]}.csv'
         csv_data = df.to_csv(index = False).encode('utf-8')
-        st.dataframe(df, use_container_width=True)
 
-        st.download_button(
-            label = 'Download as CSV File',
-            data = csv_data,
-            file_name = csv_filename,
-            mime = 'text/csv')
+        with st.expander('View CSV file'):
+            st.dataframe(df, use_container_width=True)
+            st.download_button(label = 'Download as CSV File',
+                               data = csv_data,
+                               file_name = csv_filename,
+                               mime = 'text/csv')
 
         st.markdown('<hr>', unsafe_allow_html=True)
         st.success('Analyzing your PDF file: {}'.format(uploaded_file.name))
@@ -181,63 +190,61 @@ def display_product():
         # n = st.slider('Number of Words', min_value = 1, max_value = 4, value = 1)
         # st.write('n: {}'.format(n))
         # st.write(type(n))
-        plot_n_grams(df, 2)
+        plot_n_grams(df)
 
 
 def display_pricing():
     col1, col2, col3 = st.columns(3)
     with col1:
-        with st.expander('Free'):
-            st.write('Free Version')
+        with st.expander('Free Version'):
+            st.header('Rp 0')
+            st.write('✔️  3 Files / Day')
+            st.write('✔️  Word Cloud')
+            st.write('✔️  N-Grams')
+            st.markdown('<div style="height: 180px;">&nbsp;</div>', unsafe_allow_html=True)
     with col2:
         with st.expander('Premium'):
-            st.write('Premium Version')
+            st.header('Rp 5k')
+            st.write('✔️  Unlimited Files to Scrape')
+            st.write('✔️  Word Cloud')
+            st.write('✔️  N-Grams')
+            st.write('✔️  Semantic Analysis')
+            premium_button = st.button('Subscribe Premium Plan')
+            st.markdown('<div style="height: 87px;">&nbsp;</div>', unsafe_allow_html=True)
     with col3:
         with st.expander('Enterprise'):
-            st.write('Enterprise Version')
+            st.header('Rp 15k')
+            st.write('✔️  Unlimited Files to Scrape')
+            st.write('✔️  Word Cloud')
+            st.write('✔️  N-Grams')
+            st.write('✔️  Topic Modeling')
+            st.write('✔️  Cluster Analysis')
+            enterprise_button = st.button('Subscribe Enterprise Plan')
+            st.markdown('<div style="height: 45px;">&nbsp;</div>', unsafe_allow_html=True)
 
 def display_contact_us():
-    selected_contact = option_menu(
-        menu_title = None, # required
-        options = ['Phone', 'Email', 'Address'],
-        icons = ['phone', 'mail', 'address'],
-        menu_icon = 'cast',
-        default_index = 0,
-        orientation = 'vertical',
-        styles = {'nav-link': {'--hover-color': '#fc6b03',
-                               '--active-background-color': '#eee'},
-        'nav-link-selected': {'background-color': '#eb3502'},
-        })
+    st.write('📲 ┆ +62 895 3391 31039')
+    st.write('✉️ ┆ pdfinsight@gmail.com')
+    st.write('🏠 ┆ PDFInsight Headquarter, Jl. Jenderal Sudirman No.Kav. 1, RT.1/RW.8, Karet Tengsin, Kecamatan Tanah Abang, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10220')
 
 def render_html(filename):
-    with open(filename) as f:
-        st.markdown(f'{f.read()}', unsafe_allow_html=True)
+    with open(filename, 'r') as f:
+        html_content = f.read()
+        st.components.v1.html(html_content, height=1000)
 
 def render_css(filename):
-    with open(filename) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    with open(filename, 'r') as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 def main():
     set_page_configuration()
     display_introduction()
 
-    # render_html('html/home.html')
-    render_css('css/style.css')
+    render_css('style.css')
 
-    selected_navbar_menu = option_menu(
-        menu_title = None, # required
-        options = ['Home', 'Product', 'Pricing', 'Contact Us'],
-        icons = ['house', 'bag', 'currency-dollar', 'telephone'],
-        menu_icon = 'cast',
-        default_index = 0,
-        orientation = 'horizontal',
-        styles = {'nav-link': {'--hover-color': '#fc6b03',
-                               '--active-background-color': '#eee'},
-        'nav-link-selected': {'background-color': '#eb3502'},
-        })
-
+    selected_navbar_menu = get_selected_navbar_menu()
     if selected_navbar_menu == 'Home':
-        st.write('home')
+        display_home()
     if selected_navbar_menu == 'Product':
         display_product()
     if selected_navbar_menu == 'Pricing':
